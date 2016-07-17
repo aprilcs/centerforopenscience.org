e51276a2597c27c34fac36faf4f35a2a0e0cef86
mattclark <matt@cvillesoftware.com>
2016-07-17 19:51:54 -0400

    Update main.py

--- main.py
+++ main.py
@@ -10,9 +10,9 @@ except:
 # static_path: where are the static files? has to be within site_path
 # static_route: what is the route to access static files: <a href="/static/logo.jpg"> for instance.
 w = wysiweb.WYSIWeb(
-    site_path= './www',
-    static_path='./www/static',
-    static_route='/static',
+    site_path= '/code/www',
+    static_path='/code/www/static',
+    static_route='/code/static',
     input_encoding='utf-8'
 )
 app = w.app # Needed if you want to serve with, say, nginx
@@ -25,6 +25,6 @@ if __name__ == "__main__":
 
     # or
 
-     static_app = w.freeze('./frozen')
+     static_app = w.freeze('/code/frozen')
      #static_app.run(port=5000, debug=False) # Ready to serve Flask app that mimics a static server
 
